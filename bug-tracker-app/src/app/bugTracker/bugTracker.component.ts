9aff73ece006510d6b131017420565328028cffc
Magesh <tkmagesh77@gmail.com>
2018-05-17 16:21:56 +0530

    sort pipe implementation

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -9,9 +9,14 @@ import { Bug } from './models/Bug';
 })
 export class BugTrackerComponent{
 	bugs : Bug[] = [];
+	bugSortBy : string = 'name';
+	bugSortDescending : boolean = false;
 
 	constructor(private bugOperations : BugOperationsService){
-
+		this.bugs.push(this.bugOperations.createNew('Server communication failure'));
+		this.bugs.push(this.bugOperations.createNew('Data integrity checks failed'));
+		this.bugs.push(this.bugOperations.createNew('User actions not recognized'));
+		this.bugs.push(this.bugOperations.createNew('Application not responding'));
 	}
 	onAddNewClick(bugName){
 		let newBug : Bug = this.bugOperations.createNew(bugName);
---
+++ bug-tracker-app/src/app/bugTracker/pipes/sort.pipe.ts
@@ -0,0 +1,33 @@
+import { Pipe, PipeTransform} from '@angular/core';
+
+interface Comparer{
+	(item1 : any, item2 : any) : number
+}
+
+@Pipe({
+	name : 'sort'
+})
+export class SortPipe implements PipeTransform{
+
+	private getDescendingComparerFor(comparer : Comparer) : Comparer{
+		return function(item1, item2){
+			return comparer(item1, item2) * -1;
+	    }
+	}
+	private getComparerFor(attName : string) : Comparer{
+		return function(item1, item2){
+			if (item1[attName] < item2[attName]) return -1;
+			if (item1[attName] > item2[attName]) return 1;
+			return 0;
+	    }
+	}
+	transform(data : any[], attrName : string, isDescending : boolean = false) : any[] {
+		if (!attrName) return data;
+		let comparer = this.getComparerFor(attrName);
+		if (isDescending){
+			comparer = this.getDescendingComparerFor(comparer)
+		}
+		return data.sort(comparer);
+	}
+}
+
